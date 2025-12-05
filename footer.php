65e1c5ef2da47b224626c7c4503c3a0f99fdeeb7
selvalakshmi <selvakrish141@gmail.com>
2025-12-05 17:35:57 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,137 @@
+ <!--<< Footer Section Start >>-->
+        <footer class="footer-section section-bg fix" id="contact">
+            <div class="frame-shape style-2">
+                <img src="assets/img/frame-2.png" alt="shape-img">
+            </div>
+            <div class="frame-shape-3 float-bob-y">
+                <img src="assets/img/frame-3.png" alt="shape-img">
+            </div>
+        <div class="footer-widgets-wrapper">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-3 col-lg-4 col-md-6 wow fadeInUp" data-wow-delay=".3s">
+                        <div class="single-footer-widget">
+                            <div class="widget-head">
+                                <a href="index.php">
+                                    <img src="assets/img/logo/logo.svg" alt="logo-img">
+                                </a>
+                            </div>
+                            <div class="footer-content style-two">
+                                <p>
+                                    "Playful and durable furniture designed for joyful learning spaces."
+                                </p>
+                                <div class="social-icon d-flex align-items-center">
+                                    <a href="#"><i class="fab fa-facebook-f"></i></a>
+                                    <a href="#"><i class="fab fa-twitter"></i></a>
+                                    <a href="#"><i class="fa-brands fa-linkedin-in"></i></a>
+                                    <a href="#"><i class="fa-brands fa-youtube"></i></a>
+                                </div>
+                                <a href="#contact" class="theme-btn">contact Us<i class="fa-solid fa-arrow-right-long"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-2 col-lg-4 col-md-6 ps-lg-5 wow fadeInUp" data-wow-delay=".5s">
+                        <div class="single-footer-widget">
+                            <div class="widget-head">
+                                <h3>Quick Links</h3>
+                            </div>
+                            <ul class="list-area">
+                                <li>
+                                    <a href="index.php">
+                                        <i class="fa-solid fa-chevron-right"></i>
+                                        Home
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#about">
+                                        <i class="fa-solid fa-chevron-right"></i>
+                                        About Us
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#product">
+                                        <i class="fa-solid fa-chevron-right"></i>
+                                        Product
+                                    </a>
+                                </li>
+                                   <li>
+                                    <a href="#testimonal">
+                                        <i class="fa-solid fa-chevron-right"></i>
+                                        Testimonal
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#contact">
+                                        <i class="fa-solid fa-chevron-right"></i>
+                                        Contact Us
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-lg-4 col-md-6 ps-lg-5 wow fadeInUp" data-wow-delay=".5s">
+                        <div class="single-footer-widget style-margin">
+                            <div class="widget-head">
+                                <h3>Branches</h3>
+                            </div>
+                            <ul class="list-area">
+                               <address>
+                                   sm-products,salem.
+                               </address>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-lg-4 col-md-6 wow fadeInUp" data-wow-delay=".7s">
+                        <div class="single-footer-widget style-margin">
+                            <div class="widget-head">
+                                <h3>Location</h3>
+                            </div>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d62519.58747029014!2d78.11042975478391!3d11.660766517801543!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1ssalem%20bus%20stand!5e0!3m2!1sen!2sin!4v1760352355058!5m2!1sen!2sin" width="300" height="250" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer-bottom">
+            <div class="f-bottom-shape">
+                <img src="assets/img/footer-bottom.png" alt="shape-img">
+            </div>
+            <div class="container">
+                <div class="footer-wrapper d-flex align-items-center justify-content-between">
+                    <p class="wow fadeInLeft color-2" data-wow-delay=".3s">
+                        © All Rights Reserved|Desinged and Developed by Firstmatrix Solutions<a href="index.php"></a>
+                    </p>
+               
+                </div>
+            </div>
+            <a href="#" id="scrollUp" class="scroll-icon">
+                <i class="far fa-arrow-up"></i>
+            </a>
+        </div>
+        </footer>
+
+        <!--<< All JS Plugins >>-->
+        <script src="assets/js/jquery-3.7.1.min.js"></script>
+        <!--<< Viewport Js >>-->
+        <script src="assets/js/viewport.jquery.js"></script>
+        <!--<< Bootstrap Js >>-->
+        <script src="assets/js/bootstrap.bundle.min.js"></script>
+        <!--<< Nice Select Js >>-->
+        <script src="assets/js/jquery.nice-select.min.js"></script>
+        <!--<< Waypoints Js >>-->
+        <script src="assets/js/jquery.waypoints.js"></script>
+        <!--<< Counterup Js >>-->
+        <script src="assets/js/jquery.counterup.min.js"></script>
+        <!--<< Swiper Slider Js >>-->
+        <script src="assets/js/swiper-bundle.min.js"></script>
+        <!--<< MeanMenu Js >>-->
+        <script src="assets/js/jquery.meanmenu.min.js"></script>
+        <!--<< Magnific Popup Js >>-->
+        <script src="assets/js/jquery.magnific-popup.min.js"></script>
+        <!--<< Wow Animation Js >>-->
+        <script src="assets/js/wow.min.js"></script>
+        <!--<< Main.js >>-->
+        <script src="assets/js/main.js"></script>
+    </body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,261 @@
+<!DOCTYPE html>
+<html lang="en">
+    <!--<< Header Area >>-->
+    <head>
+        <!-- ========== Meta Tags ========== -->
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="author" content="">
+        <meta name="description" content="">
+        <!-- ======== Page title ============ -->
+        <title>SM-Play School</title>
+        <!--<< Favcion >>-->
+        <link rel="shortcut icon" href="assets/img/favicon.svg">
+        <!--<< Bootstrap min.css >>-->
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+        <!--<< All Min Css >>-->
+        <link rel="stylesheet" href="assets/css/all.min.css">
+        <!--<< Animate.css >>-->
+        <link rel="stylesheet" href="assets/css/animate.css">
+        <!--<< Icomoon.css >>-->
+        <link rel="stylesheet" href="assets/css/icomoon.css">
+        <!--<< Magnific Popup.css >>-->
+        <link rel="stylesheet" href="assets/css/magnific-popup.css">
+        <!--<< MeanMenu.css >>-->
+        <link rel="stylesheet" href="assets/css/meanmenu.css">
+        <!--<< Swiper Bundle.css >>-->
+        <link rel="stylesheet" href="assets/css/swiper-bundle.min.css">
+        <!--<< Nice Select.css >>-->
+        <link rel="stylesheet" href="assets/css/nice-select.css">
+        <!--<< Main.css >>-->
+        <link rel="stylesheet" href="assets/css/main.css">
+    </head>
+    <body>
+
+        <!-- Preloader Start -->
+        <!--<div id="preloader" class="preloader">-->
+        <!--    <div class="animation-preloader">-->
+        <!--        <div class="spinner">                -->
+        <!--        </div>-->
+        <!--        <div class="txt-loading">-->
+        <!--            <span data-text-preloader="K" class="letters-loading">-->
+        <!--                K-->
+        <!--            </span>-->
+        <!--            <span data-text-preloader="I" class="letters-loading">-->
+        <!--                I-->
+        <!--            </span>-->
+        <!--            <span data-text-preloader="D" class="letters-loading">-->
+        <!--                D-->
+        <!--            </span>-->
+        <!--            <span data-text-preloader="S" class="letters-loading">-->
+        <!--                S-->
+        <!--            </span>-->
+        <!--            <span data-text-preloader="A" class="letters-loading">-->
+        <!--                A-->
+        <!--            </span>-->
+        <!--        </div>-->
+        <!--        <p class="text-center">Loading</p>-->
+        <!--    </div>-->
+        <!--    <div class="loader">-->
+        <!--        <div class="row">-->
+        <!--            <div class="col-3 loader-section section-left">-->
+        <!--                <div class="bg"></div>-->
+        <!--            </div>-->
+        <!--            <div class="col-3 loader-section section-left">-->
+        <!--                <div class="bg"></div>-->
+        <!--            </div>-->
+        <!--            <div class="col-3 loader-section section-right">-->
+        <!--                <div class="bg"></div>-->
+        <!--            </div>-->
+        <!--            <div class="col-3 loader-section section-right">-->
+        <!--                <div class="bg"></div>-->
+        <!--            </div>-->
+        <!--        </div>-->
+        <!--    </div>-->
+        <!--</div>-->
+
+        <!-- Offcanvas Area Start -->
+        <div class="fix-area">
+        <div class="offcanvas__info">
+            <div class="offcanvas__wrapper">
+                <div class="offcanvas__content">
+                    <div class="offcanvas__top mb-5 d-flex justify-content-between align-items-center">
+                        <div class="offcanvas__logo">
+                            <a href="index.php">
+                                <img src="assets/img/logo/logo.svg" alt="logo-img">
+                            </a>
+                        </div>
+                        <div class="offcanvas__close">
+                            <button>
+                            <i class="fas fa-times"></i>
+                            </button>
+                        </div>
+                    </div>
+                    <p class="text d-none d-xl-block">
+                        Nullam dignissim, ante scelerisque the  is euismod fermentum odio sem semper the is erat, a feugiat leo urna eget eros. 
+                        Duis Aenean a imperdiet risus.
+                    </p>
+                    <div class="mobile-menu fix mb-3"></div>
+                    <div class="offcanvas__contact">
+                        <h4>Contact Info</h4>
+                        <ul>
+                            <li class="d-flex align-items-center">
+                                <div class="offcanvas__contact-icon">
+                                    <i class="fal fa-map-marker-alt"></i>
+                                </div>
+                                <div class="offcanvas__contact-text">
+                                    <a target="_blank" href="#">SM-Products,Salem</a>
+                                </div>
+                            </li>
+                            <li class="d-flex align-items-center">
+                                <div class="offcanvas__contact-icon mr-15">
+                                    <i class="fal fa-envelope"></i>
+                                </div>
+                                <div class="offcanvas__contact-text">
+                                    <a href="mailto:sm-toyst@gmail.com"><span class="mailto:sm-toys@gmail.com">sm-toys@gmail.com</span></a>
+                                </div>
+                            </li>
+                            <li class="d-flex align-items-center">
+                                <div class="offcanvas__contact-icon mr-15">
+                                    <i class="fal fa-clock"></i>
+                                </div>
+                                <div class="offcanvas__contact-text">
+                                    <a target="_blank" href="#">Monday-Sunday (10.00 AM - 10.00 PM)</a>
+                                </div>
+                            </li>
+                            <li class="d-flex align-items-center">
+                                <div class="offcanvas__contact-icon mr-15">
+                                    <i class="far fa-phone"></i>
+                                </div>
+                                <div class="offcanvas__contact-text">
+                                    <a href="tel:+9585515111">+9585515111</a>
+                                </div>
+                            </li>
+                        </ul>
+                        <div class="header-button mt-4">
+                            <a href="#contact" class="theme-btn text-center">
+                                <span>Contact Us<i class="fa-solid fa-arrow-right-long"></i></span>
+                            </a>
+                        </div>
+                        <div class="social-icon d-flex align-items-center">
+                            <a href="#"><i class="fab fa-facebook-f"></i></a>
+                            <a href="#"><i class="fab fa-twitter"></i></a>
+                            <a href="#"><i class="fab fa-youtube"></i></a>
+                            <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+        <div class="offcanvas__overlay"></div>
+
+        <!-- Header Top Section Start -->
+        <div class="header-top-section-3">
+            <div class="container">
+                <div class="header-top-wrapper style-2">
+                    <ul class="contact-list">
+                        <li>
+                            <i class="fal fa-map-marker-alt"></i>
+                            SM-Toys Salem.
+                        </li>
+                        <li>
+                            <i class="far fa-envelope"></i>
+                            <a href="mailto:sm-toys@gmail.com" class="link">sm-toys@gmail.com</a>
+                        </li>
+                    </ul>
+                    <div class="social-icon d-flex align-items-center">
+                        <span>Follow Us On:</span>
+                        <a href="#"><i class="fab fa-facebook-f"></i></a>
+                        <a href="#"><i class="fab fa-twitter"></i></a>
+                        <a href="#"><i class="fa-brands fa-linkedin-in"></i></a>
+                        <a href="#"><i class="fa-brands fa-youtube"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Header Section Start -->
+        <header id="header-sticky" class="header-3">
+            <div class="container">
+                <div class="mega-menu-wrapper">
+                    <div class="header-main style-2">
+                        <div class="header-left">
+                            <div class="logo">
+                                <a href="index.php" class="header-logo">
+                                    <img src="assets/img/logo/logosm.jpeg" width="70px" hight="40px" alt="logo-img">
+                                </a>
+                            </div>
+                            <div class="mean__menu-wrapper">
+                                <div class="main-menu">
+                                    <nav id="mobile-menu">
+                                        <ul>
+                                            <li class="has-dropdown active menu-thumb">
+                                                <a href="index.php">
+                                                Home 
+                                                <!-- <i class="fas fa-angle-down"></i> -->
+                                                </a>
+                                           </li>
+                                            <li>
+                                                <a href="#about">About Us</a>
+                                            </li>
+                                            <li>
+                                                <a href="#about">About Us</a>
+                                            </li>
+                                            <li>
+                                                <a href="#about">About Us</a>
+                                            </li>
+                                        
+                                            <li>
+                                                <a href="#product">
+                                                    Products
+                                                    <!--<i class="fas fa-angle-down"></i>-->
+                                                </a>
+                                             
+                                            </li>
+                                            <li class="has-dropdown">
+                                                <a href="#testinomal">
+                                                   Testimonial
+                                                    <!--<i class="fas fa-angle-down"></i>-->
+                                                </a>
+                                               
+                                            </li>
+                                            <li>
+                                                <a href="#contact">ContactUs</a>
+                                            </li>
+                                        </ul>
+                                    </nav>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="header-right d-flex justify-content-end align-items-center">
+                            <div class="author-icon">
+                                <div class="icon">
+                                     <i class="fa-solid fa-phone"></i>
+                                </div>
+                                 <div class="content">
+                                     <span>Call Us Now</span>
+                                     <h5>
+                                         <a href="tel:+9585515111">+934567890</a>
+                                     </h5>
+                                 </div>
+                             </div>
+                            <div class="header-button">
+                                <a href="#contact" class="theme-btn">
+                                    <span>
+                                       Get More Info
+                                        <i class="fa-solid fa-arrow-right-long"></i>
+                                    </span>
+                                </a>
+                            </div>
+                            <div class="header__hamburger d-xl-none my-auto">
+                                <div class="sidebar__toggle">
+                                    <i class="fas fa-bars"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
